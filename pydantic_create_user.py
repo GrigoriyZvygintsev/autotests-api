292b16b5160d3b49b271397a00528828e7318d85
Grigory <zvgrih@gmail.com>
2025-11-09 15:30:00 +0400

    Знакомство с Pydantic. Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,22 +1,22 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, UUID4
 
 
 class CreateUserRequestSchema(BaseModel):
     """Схема тела запроса для создания пользователя."""
-    email: EmailStr
-    password: str
-    last_name: str = Field(alias='lastName')
-    first_name: str = Field(alias='firstName')
-    middle_name: str = Field(alias='middleName')
+    email: EmailStr = Field(max_length=250)
+    password: str = Field(min_length=1, max_length=250)
+    last_name: str = Field(alias='lastName', min_length=1, max_length=50)
+    first_name: str = Field(alias='firstName', min_length=1, max_length=50)
+    middle_name: str = Field(alias='middleName', min_length=1, max_length=50)
 
 
 class UserSchema(BaseModel):
     """Схема данных пользователя, возвращаемая в ответе."""
-    id: str
-    email: EmailStr
-    last_name: str = Field(alias='lastName')
-    first_name: str = Field(alias='firstName')
-    middle_name: str = Field(alias='middleName')
+    id: UUID4
+    email: EmailStr = Field(max_length=250)
+    last_name: str = Field(alias='lastName', min_length=1, max_length=50)
+    first_name: str = Field(alias='firstName', min_length=1, max_length=50)
+    middle_name: str = Field(alias='middleName', min_length=1, max_length=50)
 
 
 class CreateUserResponseSchema(BaseModel):
