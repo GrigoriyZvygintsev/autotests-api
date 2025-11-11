a952fdd1217a7ee60e205dd6f9e6a24635530114
Grigory <zvgrih@gmail.com>
2025-11-12 00:18:18 +0400

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -8,18 +8,11 @@ from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
 
-from tools.fakers import get_random_email, generate_password
 
 public_users_client = get_public_users_client()
 
 # Создаем пользователя
-create_user_request = CreateUserRequestSchema(
-    email=get_random_email(),
-    password=generate_password(),
-    last_name="string",
-    first_name="string",
-    middle_name="string"
-)
+create_user_request = CreateUserRequestSchema() # автоматическая генерация данных
 create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
@@ -33,35 +26,22 @@ exercises_client = get_exercises_client(authentication_user)
 
 # Загружаем файл
 create_file_request = CreateFileRequestSchema(
-    filename="kartinka_dlay_api_klienta.png",
-    directory="courses",
     upload_file="./testdata/files/kartinka_dlay_api_klienta.png"
-)
+)#значения (directory, filename) будут генерироваться автоматически
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
 # Создаем курс
 create_course_request = CreateCourseRequestSchema(
-    title="Python",
-    max_score=200,
-    min_score=5,
-    description="Python API course - 1.1",
-    estimated_time="4 weeks",
     preview_file_id=create_file_response.file.id,
     created_by_user_id=create_user_response.user.id
-)
+) # автоматическая генерация данных
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
 # Создаем задание
 create_exercise_request = CreateExerciseRequestSchema(
-    title="Python API client guide",
     course_id=create_course_response.course.id,
-    max_score=50,
-    min_score=1,
-    order_index=1,
-    description="Python API exercise - 1.1",
-    estimated_time="1 weeks",
-)
+) # автоматическая генерация данных
 create_exercise_response = exercises_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
--- httpx_update_user.py
+++ httpx_update_user.py
@@ -5,18 +5,8 @@ import string
 
 
 # Генерация почты и пароля
-def get_random_email():
-    return f'test.{time.time()}@example.com'
-
-
-def generate_password(length=8):
-    # Буквы (верхний и нижний регистр) + цифры
-    characters = string.ascii_letters + string.digits  # Пример: 'a-z, A-Z, 0-9'
-    return ''.join(random.choice(characters) for _ in range(length))
-
-
-random_email = get_random_email()
-random_password = generate_password()
+random_email = fake.email()
+random_password = fake.password()
 
 # Создание пользователя
 body = {
@@ -56,7 +46,7 @@ user_headers = {
     "Authorization": f"Bearer {accessToken}"
 }
 body_patch = {
-    "email": get_random_email(),
+    "email": fake.email(),
     "lastName": "VAAAXovich",
     "firstName": "Magregar",
     "middleName": "Magregorovich"
