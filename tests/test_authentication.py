0b934eae9e7965e852cb5b584541eb7f4539c58a
Grigory <zvgrih@gmail.com>
2025-11-14 20:10:46 +0400

    Маркировки Pytest. Практика работы с pytest маркировками

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,3 +1,4 @@
+import pytest
 from http import HTTPStatus
 
 from clients.authentication.authentication_client import get_authentication_client
@@ -8,20 +9,20 @@ from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code, assert_is_true
 from tools.assertions.schema import validate_json_schema
 
-
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
     public_users_client = get_public_users_client()
-    request_create_user = CreateUserRequestSchema()
-    response_create_user = public_users_client.create_user(request_create_user)
+    create_user_request = CreateUserRequestSchema()
+    public_users_client.create_user(create_user_request)
 
     authentication_client = get_authentication_client()
-    request_authentication_user = LoginRequestSchema(email=request_create_user.email, password=request_create_user.password)
-    response_authentication_user = authentication_client.login_api(request_authentication_user)
-    response_authentication_user_data =  LoginResponseSchema.model_validate_json(response_authentication_user.text)
-    print(response_authentication_user_data)
+    login_request  = LoginRequestSchema(email=create_user_request.email, password=create_user_request.password)
+    login_response = authentication_client.login_api(login_request )
+    login_response_data =  LoginResponseSchema.model_validate_json(login_response.text)
 
-    assert_status_code(response_authentication_user.status_code, HTTPStatus.OK)
-    assert_login_response(response_authentication_user_data)
+    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_login_response(login_response_data)
 
-    validate_json_schema(response_authentication_user.json(), response_authentication_user_data.model_json_schema())
+    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
 
