c7281e77759ffac89894c8288ab738121fd13466
Grigory <zvgrih@gmail.com>
2025-11-23 00:59:14 +0400

    Практикуемся в написании API автотестов. Часть 2. Практика написания API автотестов для работы с курсами

--- fixtures/courses.py
+++ fixtures/courses.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 from clients.courses.courses_client import get_courses_client, CoursesClient
 from clients.courses.courses_schema import CreateCourseRequestSchema, CreateCourseResponseSchema
-from fixtures.files import FilesClient
+from fixtures.files import FileFixture
 from fixtures.users import UserFixture
 
 class CourseFixture(BaseModel):
@@ -20,8 +20,11 @@ def courses_client(function_user: UserFixture) -> CoursesClient:
 def function_course(
         courses_client: CoursesClient,
         function_user: UserFixture,
-        function_file: FilesClient
+        function_file: FileFixture
 ) -> CourseFixture:
-    request = CreateCourseRequestSchema()
+    request = CreateCourseRequestSchema(
+        preview_file_id=function_file.response.file.id,
+        created_by_user_id=function_user.response.user.id
+    )
     response = courses_client.create_course(request)
     return CourseFixture(request=request, response=response)
