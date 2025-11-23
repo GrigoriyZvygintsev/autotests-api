ac1d5d08bdc3485d9e74eb9b80369d2c6a1daa23
Grigory <zvgrih@gmail.com>
2025-11-24 00:00:58 +0400

    Практикуемся в написании API автотестов. Часть 2. Практика написания API автотестов для работы с курсами

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -6,6 +6,7 @@ from clients.courses.courses_client import CoursesClient
 from clients.courses.courses_schema import UpdateCourseResponseSchema, UpdateCourseRequestSchema, GetCoursesQuerySchema, \
     GetCoursesResponseSchema, CreateCourseResponseSchema, CreateCourseRequestSchema
 from fixtures.courses import CourseFixture
+from fixtures.files import FileFixture
 from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.courses import assert_update_course_response, assert_get_courses_response, \
@@ -50,14 +51,17 @@ class TestCourses:
     def test_create_course(
             self,
             courses_client: CoursesClient,
-            function_course: CourseFixture
+            function_user: UserFixture,
+            function_file: FileFixture
     ):
-        request = function_course.request
+        request = CreateCourseRequestSchema(
+            preview_file_id=function_file.response.file.id,
+            created_by_user_id=function_user.response.user.id
+    )
         response = courses_client.create_course_api(request)
         response_data = CreateCourseResponseSchema.model_validate_json(response.text)
 
         assert_status_code(response.status_code, HTTPStatus.OK)
-
         assert_create_course_response(request, response_data)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
